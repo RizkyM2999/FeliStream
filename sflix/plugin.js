7d80009874ff7eddfd7a6e76a339761ec6b526b1
rizkym2999 <rizkymaul0857@gmail.com>
2026-05-30 22:19:10 +0700

    fix search sflix

--- sflix/plugin.js
+++ sflix/plugin.js
@@ -11,7 +11,7 @@
     
 	async function search(query, cb) {
 	    try {
-	        const url = `${BASE_URL}/wefeed-h5-bff/web/subject/search`;
+	        const url = `${BASE_URL}/wefeed-h5api-bff/web/subject/search`;
 	        const headers = {
 	            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
 	            "Accept": "application/json, text/plain, */*",
